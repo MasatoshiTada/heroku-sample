99246e3550e7bf7d717be3acb662af3381da0d34
Masatoshi Tada <rosso.e.nero84@gmail.com>
2020-05-07 15:04:49 +0900

    show DataSourceProperties

--- src/main/java/com/example/herokusample/IndexController.java
+++ src/main/java/com/example/herokusample/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.herokusample;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     private final UserMapper userMapper;
+    private final DataSourceProperties dataSourceProperties;
 
-    public IndexController(UserMapper userMapper) {
+    public IndexController(UserMapper userMapper, DataSourceProperties dataSourceProperties) {
         this.userMapper = userMapper;
+        this.dataSourceProperties = dataSourceProperties;
     }
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("userList", userMapper.selectAll());
+        model.addAttribute("dataSource", dataSourceProperties);
         return "index";
     }
 }
